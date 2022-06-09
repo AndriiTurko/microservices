b12734bd195c526923786c3e88d8cfe584f8772a
AndriiTurko <turko@ucu.edu.ua>
2022-06-10 01:13:28 +0300

    4 lab commit

--- facade_service/main.py
+++ facade_service/main.py
@@ -1,15 +1,22 @@
 from flask import Flask, request
 from random import choice
+import hazelcast
 import requests
 import uuid
 
 app = Flask(__name__)
+client = hazelcast.HazelcastClient()
 
-message_service = "http://localhost:8082/message_service"
+queue = client.get_queue("mes-queue").blocking()
+
+message_service = (
+    "http://localhost:8082/message_service",
+    "http://localhost:8083/message_service"
+)
 logging_service = (
-    "http://localhost:8083/logging_service",
     "http://localhost:8084/logging_service",
-    "http://localhost:8085/logging_service"
+    "http://localhost:8085/logging_service",
+    "http://localhost:8086/logging_service"
 )
 
 
@@ -17,18 +24,21 @@ logging_service = (
 def facade():
     if request.method == 'POST':
         data = {
-            "msg": request.get_data(),
+            "msg": request.get_json(),
             "uuid": str(uuid.uuid4())
         }
+
+        queue.put(data)
         log_response = requests.post(choice(logging_service), data=data)
 
-        return str(log_response.status_code) + '\n'
+        return log_response.text
 
     elif request.method == 'GET':
         log_response = requests.get(choice(logging_service)).text
-        msg_response = requests.get(message_service).text
+        msg_response = requests.get(choice(message_service)).text
 
         response = "logging-service response: " + log_response + "\n" + "messages-service response: " + msg_response
+
         return response
 
 
--- messages-service/main.py
+++ messages-service/main.py
@@ -1,12 +1,23 @@
 from flask import Flask
+import hazelcast
+import sys
+
 
 app = Flask(__name__)
+client = hazelcast.HazelcastClient()
+
+queue = client.get_queue("mes-queue").blocking()
+messages_data = []
 
 
-@app.route('/message_service', methods=['GET'])
+@app.route('/message_service', methods=['POST', 'GET'])
 def messages():
-    return "messages-service is not implemented yet..."
+    while not queue.is_empty():
+        messages_data.append(queue.take())
+        print("Received message:", messages_data[-1])
+
+    return str(messages_data)
 
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=8082)
+    app.run(host="localhost", port=int(sys.argv[1]))
